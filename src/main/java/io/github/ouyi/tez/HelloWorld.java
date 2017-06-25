5fa250c295087d1201a1fb97aa67a5aed3a4132a
Yi Ou <ouyi@gmx.de>
2017-06-25 11:22:37 +0000

    Create TezClient instance

--- src/main/java/io/github/ouyi/tez/HelloWorld.java
+++ src/main/java/io/github/ouyi/tez/HelloWorld.java
@@ -1,16 +1,29 @@
 package io.github.ouyi.tez;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.tez.client.TezClient;
+import org.apache.tez.dag.api.TezConfiguration;
+import org.apache.tez.dag.api.TezException;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class HelloWorld extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
+        TezClient tezClient = createTezClient(new TezConfiguration(getConf() == null ? new Configuration() : getConf()));
+        System.out.println(tezClient);
         System.out.println(Arrays.toString(args));
         return 0;
+    }
+
+    private TezClient createTezClient(TezConfiguration tezConf) throws IOException, TezException {
+        TezClient tezClient = TezClient.create(getClass().getSimpleName(), tezConf);
+        tezClient.start();
+        return tezClient;
     }
 
     public static void main(String[] args) throws Exception {
