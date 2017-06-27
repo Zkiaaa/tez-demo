a2e7087fe8636fd9b5af398b52d99a8dd6cbb0ef
Yi Ou <ouyi@gmx.de>
2017-06-27 21:20:02 +0000

    Create dummy DAG

--- src/main/java/io/github/ouyi/tez/HelloWorld.java
+++ src/main/java/io/github/ouyi/tez/HelloWorld.java
@@ -1,29 +1,81 @@
 package io.github.ouyi.tez;
 
+import com.google.common.collect.Sets;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.tez.client.CallerContext;
 import org.apache.tez.client.TezClient;
+import org.apache.tez.dag.api.DAG;
 import org.apache.tez.dag.api.TezConfiguration;
 import org.apache.tez.dag.api.TezException;
+import org.apache.tez.dag.api.client.DAGClient;
+import org.apache.tez.dag.api.client.DAGStatus;
+import org.apache.tez.dag.api.client.StatusGetOpts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Set;
 
 public class HelloWorld extends Configured implements Tool {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
+
     @Override
     public int run(String[] args) throws Exception {
-        TezClient tezClient = createTezClient(new TezConfiguration(getConf() == null ? new Configuration() : getConf()));
-        System.out.println(tezClient);
         System.out.println(Arrays.toString(args));
+
+        Configuration conf = getConf();
+        TezConfiguration tezConf = new TezConfiguration(conf == null ? new Configuration() : conf);
+
+        TezClient tezClient = TezClient.create(getClass().getSimpleName(), tezConf);
+        System.out.println(tezClient);
+
+        String inputPath = args[0];
+        String outputPath = args[1];
+        int numPartitions = Integer.parseInt(args.length > 2 ? args[2] : "1");
+        DAG dag = createDAG(conf, tezConf, inputPath, outputPath, numPartitions);
+        System.out.println(dag);
+
+        //runDag(dag, tezClient, LOGGER);
         return 0;
     }
 
-    private TezClient createTezClient(TezConfiguration tezConf) throws IOException, TezException {
-        TezClient tezClient = TezClient.create(getClass().getSimpleName(), tezConf);
-        tezClient.start();
-        return tezClient;
+    private DAG createDAG(Configuration conf, TezConfiguration tezConf, String inputPath, String outputPath, int numPartitions) {
+        DAG dag = DAG.create("HelloWorld DAG");
+        return dag;
+    }
+
+    public int runDag(DAG dag, TezClient tezClient, Logger logger) throws TezException,
+            InterruptedException, IOException {
+        try {
+            tezClient.start();
+            tezClient.waitTillReady();
+
+            // Set up caller context
+            ApplicationId appId = tezClient.getAppMasterApplicationId();
+            CallerContext callerContext = CallerContext.create("HelloWorldContext", "Caller id: " + appId, "HelloWorldType", "Tez HelloWorld DAG: " + dag.getName());
+            dag.setCallerContext(callerContext);
+
+            // Submit DAG and wait for completion
+            DAGClient dagClient = tezClient.submitDAG(dag);
+            Set<StatusGetOpts> statusGetOpts = Sets.newHashSet(StatusGetOpts.GET_COUNTERS);
+            DAGStatus dagStatus = dagClient.waitForCompletionWithStatusUpdates(statusGetOpts);
+
+            // Check status
+            if (dagStatus.getState() == DAGStatus.State.SUCCEEDED) {
+                return 0;
+            } else {
+                logger.info("DAG diagnostics: " + dagStatus.getDiagnostics());
+                return -1;
+            }
+        } finally {
+            tezClient.stop();
+        }
     }
 
     public static void main(String[] args) throws Exception {
